22865deb2aa0ee6a9a759fac57dde866229bd267
carlosgprado <carlos.g.prado@gmail.com>
2016-10-28 13:11:40 +0200

    Fixed static files issue

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '/static')
 socketio = SocketIO(app)
 
 connected_clients = 0
